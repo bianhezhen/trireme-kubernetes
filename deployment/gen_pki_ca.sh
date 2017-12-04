8ea4e0077b9b3e486f5f88c2a8986927fbd57a44
Bernard Van De Walle <bernard@bvandewa.com>
2017-12-04 09:12:52 -0800

    New: Reuse .pem if already generated and warn user (#121)

--- deployment/gen_pki_ca.sh
+++ deployment/gen_pki_ca.sh
@@ -5,6 +5,14 @@ NAME="ca"
 SECRET_NAME="trireme-cacert"
 TRIREME_CA_PASS_SECRET_ENTRY="ca-pass"
 
-echo "Attempting to generate PKI"
-tg cert --is-ca --auth-client --auth-server --pass $PASS --name $NAME
+echo "CA generation script"
+echo "For production use -- Use your own CA or generate one securely"
+
+if [ -f $NAME-cert.pem ]; then
+    echo "CA seems to already exist. Reusing"
+else
+    echo "Attempting to generate PKI"
+    tg cert --is-ca --auth-client --auth-server --pass $PASS --name $NAME
+fi
+
 kubectl --namespace kube-system create secret generic $SECRET_NAME --from-file=$NAME-cert.pem --from-file=$NAME-key.pem --from-literal=$TRIREME_CA_PASS_SECRET_ENTRY=$PASS
