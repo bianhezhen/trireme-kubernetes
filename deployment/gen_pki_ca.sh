017101b50458fe746faf04fa02320cc372ac7da2
Bernard Van De Walle <bernard@bvandewa.com>
2017-12-04 09:47:15 -0800

    Fix: CACertPass Env variable message added

--- deployment/gen_pki_ca.sh
+++ deployment/gen_pki_ca.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PASS=$(openssl rand -base64 16)
+CACERT_PASS=$(openssl rand -base64 16)
 NAME="ca"
 SECRET_NAME="trireme-cacert"
 TRIREME_CA_PASS_SECRET_ENTRY="ca-pass"
@@ -10,9 +10,10 @@ echo "For production use -- Use your own CA or generate one securely"
 
 if [ -f $NAME-cert.pem ]; then
     echo "CA seems to already exist. Reusing"
+    echo "Make sure your CA Encryption pass is set as CACERT_PASS env variable"
 else
     echo "Attempting to generate PKI"
-    tg cert --is-ca --auth-client --auth-server --pass $PASS --name $NAME
+    tg cert --is-ca --auth-client --auth-server --pass $CACERT_PASS --name $NAME
 fi
 
-kubectl --namespace kube-system create secret generic $SECRET_NAME --from-file=$NAME-cert.pem --from-file=$NAME-key.pem --from-literal=$TRIREME_CA_PASS_SECRET_ENTRY=$PASS
+kubectl --namespace kube-system create secret generic $SECRET_NAME --from-file=$NAME-cert.pem --from-file=$NAME-key.pem --from-literal=$TRIREME_CA_PASS_SECRET_ENTRY=$CACERT_PASS
